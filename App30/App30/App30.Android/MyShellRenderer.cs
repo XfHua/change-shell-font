48cd61b496cbfc14048f60d60cf17580d4da1a64
XfHua <jasonHuaxf@163.com>
2020-06-29 17:04:34 +0800

    Add files via upload

---
+++ App30/App30/App30.Android/MyShellRenderer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Text;
+using Android.Text.Style;
+using Android.Views;
+using Android.Widget;
+using App30;
+using App30.Droid;
+using Java.Util;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(AppShell), typeof(MyShellRenderer))]
+namespace App30.Droid
+{
+    public class MyShellRenderer : ShellRenderer
+    {
+        public MyShellRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override IShellBottomNavViewAppearanceTracker CreateBottomNavViewAppearanceTracker(ShellItem shellItem)
+        {
+            return new CustomBottomNavAppearance();
+        }
+    }
+
+    public class CustomBottomNavAppearance : IShellBottomNavViewAppearanceTracker
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public void ResetAppearance(BottomNavigationView bottomView)
+        {
+
+        }
+
+        public void SetAppearance(BottomNavigationView bottomView, ShellAppearance appearance)
+        {
+
+            IMenu menu = bottomView.Menu;
+            for (int i = 0; i < bottomView.Menu.Size(); i++)
+            {
+                IMenuItem menuItem = menu.GetItem(i);
+                var title = menuItem.TitleFormatted;
+                SpannableStringBuilder sb = new SpannableStringBuilder(title);
+
+                int a = sb.Length();
+
+                sb.SetSpan(new AbsoluteSizeSpan(20,true), 0, a, SpanTypes.ExclusiveExclusive);
+
+                menuItem.SetTitle(sb);
+            }
+
+        }
+    }
+}
